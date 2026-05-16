20f5658dc19418b425ad3d80169a7cd68a946817
yared aregayehu <yaredbaby21@gmail.com>
2026-05-15 19:31:49 -0500

    `Updated questionRoute.js with new imports and route definitions`

--- routes/quesionRoute.js
+++ routes/quesionRoute.js
@@ -1,8 +1,15 @@
 import express from "express";
+import {
+  getAllQuestions,
+  getSingleQuestion,
+  postQuestion,
+} from "../controllers/questionController.js";
+import authMiddleware from "../middleware/auth.js";
+
 const router = express.Router();
 
-
-router.get("/all-questions", (req, res) => {
-    res.send("all questions route");});
+router.get("/", getAllQuestions);
+router.get("/:question_id", getSingleQuestion);
+router.post("/", authMiddleware, postQuestion);
 
 export default router;
