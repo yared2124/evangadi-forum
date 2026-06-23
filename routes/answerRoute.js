71e21c0a0dc72aeb65c6081173c030d4eb894767
yared aregayehu <yaredbaby21@gmail.com>
2026-06-23 14:28:12 -0500

    `Added getSingleAnswer route and updated answerController exports`

--- routes/answerRoute.js
+++ routes/answerRoute.js
@@ -1,13 +1,22 @@
 import express from "express";
 import {
   getAnswersForQuestion,
+  getSingleAnswer,
   postAnswer,
-} from "../controller/answerController.js";
-import authMiddleware from "../Middleware/authMiddleware.js";
+  updateAnswer,
+  deleteAnswer,
+} from "../controllers/answerController.js";
+import authMiddleware from "../middleware/auth.js";
 
 const router = express.Router();
 
+// Public routes
 router.get("/:question_id", getAnswersForQuestion);
+router.get("/single/:answer_id", getSingleAnswer);
+
+// Protected routes
 router.post("/", authMiddleware, postAnswer);
+router.put("/:answer_id", authMiddleware, updateAnswer);
+router.delete("/:answer_id", authMiddleware, deleteAnswer);
 
 export default router;
